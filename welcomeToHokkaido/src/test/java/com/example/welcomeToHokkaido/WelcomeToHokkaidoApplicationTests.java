d195db9bd4358f28c6cf7351b0a87e239f68ff00
DongwonLim <idongwon01@gmail.com>
2025-03-19 15:54:27 +0900

    250319 first commit!!

---
+++ welcomeToHokkaido/src/test/java/com/example/welcomeToHokkaido/WelcomeToHokkaidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.welcomeToHokkaido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WelcomeToHokkaidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
